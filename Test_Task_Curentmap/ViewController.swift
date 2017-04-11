e91866d324e0ffcc3867f25dd8c25435cb84735b
vlad5ss <vlad5grup@gmail.com>
2017-04-11 14:46:05 +0400

    Initial Commit

---
+++ Test_Task_Curentmap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_Task_Curentmap
+//
+//  Created by mac on 4/11/17.
+//  Copyright © 2017 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
